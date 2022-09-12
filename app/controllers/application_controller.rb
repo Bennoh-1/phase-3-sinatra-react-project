75f0bb01302249df22e5ba35883ac5a50f29e2b0
francis <franciskibira017@gmail.com>
2022-09-12 19:24:34 +0300

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,70 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+
+
+
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/sellers" do
+    seller = Seller.all 
+    #{ message: "Good luck with your project!" }.to_json
+    seller.to_json
   end
 
+  get "/buyers" do
+    buyer = Buyer.all
+    buyer.to_json
+  end
+  
+ # get "/commodities" do
+   # commodity = Commodity.all
+   # commodity.to_json
+  
+
+
+
+# POST
+
+post '/sellers' do
+  seller = Seller.create(
+    name: params[:name],
+    speciality: params[:speciality]   
+  )
+  seller.to_json
 end
+
+post '/buyers' do
+  buyers= Buyer.create(
+    name:params[:name],
+    gender:params[:gender],
+    contact:params[:contact],
+  )
+  buyers.to_json
+end
+
+
+
+post '/commodities' do
+  commodities= Seller.create(
+    name:params[:name],
+    type:params[:type],
+    seller_id:params[:seller_id],
+    buyer_id:params[:buyer_id],
+  )
+  commodities.to_json
+end
+#DELETE
+
+delete '/buyers/:id' do
+  buyer = Buyer.find(params[:buyer])
+  buyer.destroy
+  buyer.to_json
+end
+
+end
+
+
+
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,11 @@ sllr3 = Seller.create(name:"devine", speciality:"shirt")
 sllr4 = Seller.create(name:"yaisir", speciality:"jeans")
 sllr5 = Seller.create(name:"moha", speciality:"dress")
 
+#cmdty1 = Commodity.create(name:"shoe", type: "airfoce",  seller_id:1, buyer_id:1)
+#cmdty2 = Commodity.create(name:"t-shirt", type: "printed t-shirt",  seller_id:2, buyer_id:2)
+#cmdty3 = Commodity.create(name:"shirt", type: "floral shirt",  seller_id:3, buyer_id:3)
+#cmdty4 = Commodity.create(name:"jean", type: "slim fit",  seller_id:4, buyer_id:4)
+#cmdty5 = Commodity.create(name:"dress", type: "dinner dress",  seller_id:5, buyer_id:5)
+
 puts "seeds end ........"
 
