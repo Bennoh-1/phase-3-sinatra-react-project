5d78b5473520c8a039bb52e97c866a42cbed5325
francis <franciskibira017@gmail.com>
2022-09-14 09:45:53 +0300

    changes made

--- db/migrate/20220908123855_create_commodities_table.rb
+++ db/migrate/20220908123855_create_commodities_table.rb
@@ -1,7 +1,6 @@
 class CreateCommoditiesTable < ActiveRecord::Migration[6.1]
   def change
     create_table :commodities do |t|
-      t.string :image
       t.string :name
       t.string :type
       t.integer :seller_id
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@ puts "seeding......"
 byr1 = Buyer.create(name:"mish", gender:"male", contact:"0700456677")
 byr1 = Buyer.create(name:"evalyne", gender:"female", contact:"0788993311")
 byr1 = Buyer.create(name:"mhesh", gender:"male", contact:"0733227788")
+byr1 = Buyer.create(name:"Frank", gender:"male", contact:"0733245788")
 
 sllr1 = Seller.create(name:"lockey", speciality:"shoes")
 sllr2 = Seller.create(name:"franc", speciality:"t-shirt")
@@ -10,11 +11,11 @@ sllr3 = Seller.create(name:"devine", speciality:"shirt")
 sllr4 = Seller.create(name:"yaisir", speciality:"jeans")
 sllr5 = Seller.create(name:"moha", speciality:"dress")
 
-#cmdty1 = Commodity.create(name:"shoe", type: "airfoce",  seller_id:1, buyer_id:1)
-#cmdty2 = Commodity.create(name:"t-shirt", type: "printed t-shirt",  seller_id:2, buyer_id:2)
-#cmdty3 = Commodity.create(name:"shirt", type: "floral shirt",  seller_id:3, buyer_id:3)
-#cmdty4 = Commodity.create(name:"jean", type: "slim fit",  seller_id:4, buyer_id:4)
-#cmdty5 = Commodity.create(name:"dress", type: "dinner dress",  seller_id:5, buyer_id:5)
+cmdty1 = Commodity.create(name:"shoe", type: "airfoce",seller_id: rand(0..5), buyer_id: rand(1..4))
+cmdty2 = Commodity.create(name:"t-shirt", type: "printed t-shirt", seller_id: rand(0..5), buyer_id: rand(1..4))
+cmdty3 = Commodity.create(name:"shirt", type: "floral shirt",  seller_id: rand(0..5), buyer_id: rand(1..4))
+cmdty4 = Commodity.create(name:"jean", type: "slim fit", seller_id: rand(0..5), buyer_id: rand(1..4))
+cmdty5 = Commodity.create(name:"dress", type: "dinner dress", seller_id: rand(0..5), buyer_id: rand(1..4))
 
 puts "seeds end ........"
 
