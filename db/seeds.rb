fcd698053dd798e6353459393696316485cf741f
francis <franciskibira017@gmail.com>
2022-09-14 10:00:10 +0300

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ sllr3 = Seller.create(name:"devine", speciality:"shirt")
 sllr4 = Seller.create(name:"yaisir", speciality:"jeans")
 sllr5 = Seller.create(name:"moha", speciality:"dress")
 
-cmdty1 = Commodity.create(name:"shoe", type: "Ndulaa" ,seller_id: rand(0..5), buyer_id: rand(1..4))
-cmdty2 = Commodity.create(name:"t-shirt", type: "printed t-shirt", seller_id: rand(0..5), buyer_id: rand(1..4))
-cmdty3 = Commodity.create(name:"shirt", type: "floral shirt",  seller_id: rand(0..5), buyer_id: rand(1..4))
-cmdty4 = Commodity.create(name:"jean", type: "slim fit", seller_id: rand(0..5), buyer_id: rand(1..4))
-cmdty5 = Commodity.create(name:"dress", type: "dinner dress", seller_id: rand(0..5), buyer_id: rand(1..4))
+#cmdty1 = Commodity.create(name:"shoe", type: "Ndulaa" ,seller_id: rand(0..5), buyer_id: rand(1..4))
+#cmdty2 = Commodity.create(name:"t-shirt", type: "printed t-shirt", seller_id: rand(0..5), buyer_id: rand(1..4))
+#cmdty3 = Commodity.create(name:"shirt", type: "floral shirt",  seller_id: rand(0..5), buyer_id: rand(1..4))
+#cmdty4 = Commodity.create(name:"jean", type: "slim fit", seller_id: rand(0..5), buyer_id: rand(1..4))
+#cmdty5 = Commodity.create(name:"dress", type: "dinner dress", seller_id: rand(0..5), buyer_id: rand(1..4))
 
 puts "seeds end ........"
 
