aa3be467946f57c1c563431e570a72203152b5d9
francis <franciskibira017@gmail.com>
2022-09-14 09:54:47 +0300

    changes made

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ sllr3 = Seller.create(name:"devine", speciality:"shirt")
 sllr4 = Seller.create(name:"yaisir", speciality:"jeans")
 sllr5 = Seller.create(name:"moha", speciality:"dress")
 
-cmdty1 = Commodity.create(name:"shoe", type: "airfoce",seller_id: rand(0..5), buyer_id: rand(1..4))
+cmdty1 = Commodity.create(name:"shoe", type: "Ndulaa" ,seller_id: rand(0..5), buyer_id: rand(1..4))
 cmdty2 = Commodity.create(name:"t-shirt", type: "printed t-shirt", seller_id: rand(0..5), buyer_id: rand(1..4))
 cmdty3 = Commodity.create(name:"shirt", type: "floral shirt",  seller_id: rand(0..5), buyer_id: rand(1..4))
 cmdty4 = Commodity.create(name:"jean", type: "slim fit", seller_id: rand(0..5), buyer_id: rand(1..4))
