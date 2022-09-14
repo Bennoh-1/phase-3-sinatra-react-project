b7fa5f0608bfc934ee58fbe883912fb917df27c4
francis <franciskibira017@gmail.com>
2022-09-14 09:34:43 +0300

    adding image url

--- db/migrate/20220908123855_create_commodities_table.rb
+++ db/migrate/20220908123855_create_commodities_table.rb
@@ -1,6 +1,7 @@
 class CreateCommoditiesTable < ActiveRecord::Migration[6.1]
   def change
     create_table :commodities do |t|
+      t.string :image
       t.string :name
       t.string :type
       t.integer :seller_id
